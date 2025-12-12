b8e70f4d3e28976a97a4caf815367b0f333f630a
Pietro Medeiros <pietrogoncalvesmedeiros@gmail.com>
2025-12-11 21:53:54 -0300

    Remover botão de tema e limpar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/ThemeProvider";
 import { Sidebar } from "@/components/Sidebar";
 import { MobileNav } from "@/components/MobileNav";
-import { ThemeToggle } from "@/components/ThemeToggle";
 import { AuthProvider } from "@/lib/auth-context";
 import { NotificationHandler } from "@/components/NotificationHandler";
 
@@ -34,27 +32,22 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 dark:bg-gray-950`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-950`}
       >
         <AuthProvider>
-          <ThemeProvider>
-            <NotificationHandler>
-              <MobileNav />
-              <div className="flex min-h-screen bg-gray-50 dark:bg-gray-950 flex-col md:flex-row">
-                <Sidebar />
-                <main className="flex-1 md:ml-20 mt-16 md:mt-0">
-                  <div className="min-h-screen p-4 md:p-8">
-                    <div className="max-w-7xl mx-auto">
-                      <div className="flex justify-end mb-8">
-                        <ThemeToggle />
-                      </div>
-                      {children}
-                    </div>
+          <NotificationHandler>
+            <MobileNav />
+            <div className="flex min-h-screen bg-white dark:bg-gray-950 flex-col md:flex-row">
+              <Sidebar />
+              <main className="flex-1 md:ml-20 mt-16 md:mt-0">
+                <div className="min-h-screen p-4 md:p-8 bg-white dark:bg-gray-950">
+                  <div className="max-w-7xl mx-auto">
+                    {children}
                   </div>
-                </main>
-              </div>
-            </NotificationHandler>
-          </ThemeProvider>
+                </div>
+              </main>
+            </div>
+          </NotificationHandler>
         </AuthProvider>
       </body>
     </html>
